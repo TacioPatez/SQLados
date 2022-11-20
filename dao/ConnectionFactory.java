7e0cd9d423c40aa309c067823619fa418232d966
TacioPatez <taciopatez@hotmail.com>
2022-11-19 22:17:13 -0300

    DAO Update

--- dao/ConnectionFactory.java
+++ dao/ConnectionFactory.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    Connection getConnection() throws SQLException {
+    static Connection getConnection() throws SQLException {
         return DriverManager.getConnection("jdbc:sqlserver://;serverName=localhost;databaseName=SQLados");
     }
 }
