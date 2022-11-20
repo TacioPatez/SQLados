287aca5b19632206335d7dbee2a84930e33e7150
TacioPatez <taciopatez@hotmail.com>
2022-11-19 21:21:35 -0300

    DAO USER (incompleto)

--- dao/UserDao.java
+++ dao/UserDao.java
@@ -2,13 +2,15 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import model.User;
 
 public class UserDao extends User {
 
-  public void register(String login, String password, String name, String sector, String birth, String CPF) throws SQLException {
+  public void register(String login, String password, String name, String sector, String birth, String CPF){
         String sql = "INSERT INTO USR" +
                     "VALUES (?,?,?,?,?,?)";
 
@@ -23,6 +25,39 @@ public class UserDao extends User {
                 smt.setString(5, birth);
                 smt.setString(6, CPF);
             smt.executeUpdate(); // Executa Comando no SQL
+            smt.close(); // Finaliza o PreparedStatement
+            con.close(); // Finaliza a Conexão com o BD
+        } catch (SQLException e){
+            System.out.println("Error ao Registrar o Usuario!");
+        }
+    }
+
+    public List<model.User> getAll(){
+        String sql = "SELECT *" +
+                    "FROM USR";
+
+        try{
+            Connection con = new ConnectionFactory().getConnection();
+            PreparedStatement smt = con.prepareStatement(sql);
+            ResultSet rs;
+            List<model.User> users = null;
+
+            rs = smt.executeQuery(); // Executa Comando no SQL e rs recebe os valores
+            
+            if(rs!=null){
+                while(rs.next()){
+                    model.User u = new User();
+                    u.setLogin(rs.getString("USR_LOGIN"));
+                    u.setPassword(rs.getString("USR_SENHA"));
+                    u.setName(rs.getString("USR_NAME"));
+                    u.setDepartment(rs.getString("USR_STR_COD"));
+                    u.setBirthDate(rs.getString("USR_BIRTH"));
+                    u.setCPF(rs.getString("USR_CPF"));
+                    users.add(u);
+                }
+            }
+
+            rs.close(); // Finaliza os dados da Query
             smt.close(); // Finaliza o PreparedStatement
             con.close(); // Finaliza a Conexão com o BD
         } catch (SQLException e){
